40b7208f73b70223b124de8f89d28f6f814952f5
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-18 10:08:40 +0100

    Exercice 3

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>(new int[] 
+            {
+                67, 98, 53, 9
+            });
+            int sum = 0;
+            string calc = "";
+            for (int y = 0; y < numbers.Count; y++)
+            {
+                sum = sum + numbers[y];
+            }
+            calc = calc + numbers[0];
+            for (int x = 1; x < numbers.Count; x++)
+            {
+                calc = calc + " + " + numbers[x];
+            }
+            calc = calc + " = " + sum;
+            Console.WriteLine(calc);
+        }
+    }
+}
